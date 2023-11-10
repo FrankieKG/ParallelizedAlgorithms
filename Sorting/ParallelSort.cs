7d6369ef4e961ed16fac76dc1e1b948bf4e38e51
Franco_K <Franco.Kovacevic.8369@student.uu.se>
2023-11-10 10:47:03 +0100

    Add project files.

---
+++ Sorting/ParallelSort.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+  // Klassen sorterar en array genom att använda flera trådar samtidigt.
+  public class ParallelSort<T> : ISort<T>
+  {
+    // Sorterings algoritmens namn och identifierare
+    public string Name => "ParallelSort";
+
+    // Standard sorteringsmetoden, som använder standard comparer
+    public void Sort(T[] inputOutput)
+    {
+      Sort(inputOutput, Comparer<T>.Default);
+    }
+
+    // Sorteringsmetod som använder comparer för att jämföra object dvs chunks
+    public void Sort(T[] inputOutput, IComparer<T> comparer)
+    {
+
+      // int som håller antalet kärnor miljön har, dvs hur många kärnor datorns processor har
+      int cores = Environment.ProcessorCount;
+      // Ränkar ut storleken av varje chunk, genom att dividera längden på arrayen med antalet kärnor
+      // inputOutput.Length + cores - 1, istället för inputOutput.Length / cores, så att varje element i arrayen blir inkluderade i en chunk, det skapar lite extra utrymme i varje chunk så att "remaindern" också kan bli includerad i chunksen
+      int chunkSize = (inputOutput.Length + cores - 1) / cores; 
+
+      // Array som används för att hålla tasks, en task för varje kärna, och arrayens längd bestäms av antalet kärnor
+      Task[] tasks = new Task[cores];
+      for (int i = 0; i < cores; i++)
+      {
+        // Start index för chunken, genom att mulitplicera i med chunksize så får vi start index av alla chunksen efter varandra, på detta sätt delegerar vi till varje kärna vart de ska börja arbeta
+        int start = i * chunkSize;
+        // Slut index för chunken, inputOutput.Length används här för att se till att slut index inte överstiger det totala antalet element i arrayen
+        int end = Math.Min(start + chunkSize, inputOutput.Length);
+        // Task för att sortera varje chunk av arrayen parallelt, detta genom att den först tar in arrayen som ska sorteras "inputOutput" och sedan sorterar från start (start index) och sedan sorterar det antalet element som angivits ska sorteras, det får den genom "end - start", sedan används comparer för att sköta sorteringen
+        // Genom att använda Task.Run delegeras varje sorterings operation till en separat tråd, vilket tillåter att flera chunks blir sorterade samtidigt
+        tasks[i] = Task.Run(() => Array.Sort(inputOutput, start, end - start, comparer));
+      }
+      
+      // Vänta tills alla tasks är slutförda
+      Task.WaitAll(tasks);
+      
+      // Merga alla sorterade chunks till den ursprungliga arrayen
+      MergeSortedChunks(inputOutput, cores, chunkSize, comparer);
+    }
+
+    // Metod för att merga de sorterade chunkesn från varje task till en sorterad array
+    private void MergeSortedChunks(T[] inputOutput, int cores, int chunkSize, IComparer<T> comparer)
+    {
+
+      // Array för att hålla de mergade och sorterade elementen
+      T[] merged = new T[inputOutput.Length];
+
+      // Använder mig av SortedSet som själv sorterar sitt innehåll, med det minsta först och största sist
+      // value representerar värdet vi ska jämföra, chunkindex representerar vilken chunk som value är i, och elementIndex representerar vilket index i chunken som value sitter på
+      // comparer jämför det minsta värdet i varje chunkarray för att bestämma i vilken ordning de ska vara i den slutgiltiga mergade arrayen
+      // Vi fortsätter att plocka ut de minsta värdena från chunkarrayerna tills att dessa är tomma
+      var minChunk = new SortedSet<(T value, int chunkIndex, int elementIndex)>(Comparer<(T value, int chunkIndex, int elementIndex)>.Create((a, b) =>
+      {
+
+        int compare = comparer.Compare(a.value, b.value);
+        if (compare != 0) return compare;
+        return a.chunkIndex.CompareTo(b.chunkIndex);
+      })
+      );
+
+      // Skapar sorted set med det första elementet av varje chunk, varje chunk är redan sorterad, so första värdet av varje chunk är oche dess minsta värde
+      for(int i = 0; i < cores; i++)
+      {
+        int start = i * chunkSize;
+        if(start < inputOutput.Length)
+        {
+          minChunk.Add((inputOutput[start], i, start + 1));
+        }
+      }
+
+      // Vi itererar genom hela längden av den ursprungliga arrayen för att bygga den sorterade merged arrayen
+      for(int i = 0; i < inputOutput.Length; i++)
+      {
+        // Får minsta värdet av varje sortedset
+        var (value, chunkIndex, elementIndex) = minChunk.Min;
+
+        // När vi har fått värdet så tar vi bort det från sortedsettet för att upreppa processen
+        minChunk.Remove(minChunk.Min);
+        // Vi lägger till det minsta värdet till den mergade arrayen 
+        merged[i] = value;
+
+        // Räknar ut start och slut index för nästa element från chunken
+        int nextChunkStart = chunkIndex * chunkSize;
+        int nextChunkEnd = Math.Min(nextChunkStart + chunkSize, inputOutput.Length);
+        
+        // Om inte chunken är tom så läggs nästa dvs det minsta värdet av chunken (eftersom alla chunks är sorterade med minst först) till sortedsettet
+        if(elementIndex < nextChunkEnd)
+        {
+          minChunk.Add((inputOutput[elementIndex], chunkIndex, elementIndex + 1));
+        }
+      }
+      // Använder oss av Copy för att kopiera från source arrayen "merged" till destination arrayen "inputOutput" där vi också deklarerar hur många element som ska kopieras över med hjälp av "merged.Length"
+      Array.Copy(merged, inputOutput, merged.Length);
+    }
+  }
+}
---
+++ Sorting/ParallelTopNSort.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Sorting
+{
+  public class ParallelTopNSort<T> : ITopNSort<T>
+  {
+    public string Name => "ParallelTopNSort";
+
+    public T[] TopNSort(T[] inputOutput, int n)
+    {
+      return TopNSort(inputOutput, n, Comparer<T>.Default);
+    }
+
+    public T[] TopNSort(T[] inputOutput, int n, IComparer<T> comparer)
+    {
+      if ( n <=0 || inputOutput.Length == 0)
+      {
+        return new T[0];
+      }
+
+      n = Math.Min(n, inputOutput.Length);
+
+      int cores = Environment.ProcessorCount;
+      int chunkSize = (inputOutput.Length + cores - 1) / cores;
+
+      T[][] sortedChunks = new T[cores][];
+      Task[] tasks = new Task[cores];
+
+      for (int i = 0; i < cores; i++)
+      {
+        int localIndex = i; 
+        tasks[i] = Task.Run(() =>
+        {
+          int start = localIndex * chunkSize;
+          int end = (localIndex == cores - 1) ? inputOutput.Length : start + chunkSize;
+          int chunkLength = end - start;
+
+          T[] chunk = new T[chunkLength];
+          Array.Copy(inputOutput, start, chunk, 0, chunkLength);
+          Array.Sort(chunk, comparer);
+          sortedChunks[localIndex] = chunk;
+        });
+      }
+
+      Task.WaitAll(tasks);
+
+      T[] topN = MergeSortedChunks(sortedChunks, n, comparer);
+      return topN;
+    }
+
+    public class MinElements<TValue, TComparer> where TComparer : IComparer<TValue>
+    {
+      private List<TValue> elements = new List<TValue>();
+      private List<int> chunkIndices = new List<int>();
+      private List<int> elementIndices = new List<int>();
+      private TComparer comparer;
+
+      public MinElements(TComparer comparer)
+      {
+        this.comparer = comparer;
+      }
+
+      public int Count => elements.Count;
+
+      public (TValue value, int chunkIndex, int elementIndex) RemoveMin()
+      {
+        if (elements.Count == 0)
+        {
+          throw new InvalidOperationException("Heap is empty");
+        }
+        var minValue = elements[0];
+        var minChunkIndex = chunkIndices[0];
+        var minElementIndex = elementIndices[0];
+        Swap(0, elements.Count - 1);
+        elements.RemoveAt(elements.Count - 1);
+        chunkIndices.RemoveAt(chunkIndices.Count - 1);
+        elementIndices.RemoveAt(elementIndices.Count - 1);
+        elementsDown(0);
+        return (minValue, minChunkIndex, minElementIndex);
+      }
+
+      public void Add(TValue value, int chunkIndex, int elementIndex)
+      {
+        elements.Add(value);
+        chunkIndices.Add(chunkIndex);
+        elementIndices.Add(elementIndex);
+        elementsUp(elements.Count - 1);
+      }
+
+      private void elementsUp(int index)
+      {
+        while (index > 0)
+        {
+          int parentIndex = (index - 1) / 2;
+          if (comparer.Compare(elements[index], elements[parentIndex]) >= 0)
+          {
+            break;
+          }
+          Swap(index, parentIndex);
+          index = parentIndex;
+        }
+      }
+
+      private void elementsDown(int index)
+      {
+        int smallest = index;
+        int leftChildIndex = 2 * index + 1;
+        int rightChildIndex = 2 * index + 2;
+
+        if (leftChildIndex < elements.Count && comparer.Compare(elements[leftChildIndex], elements[smallest]) < 0)
+        {
+          smallest = leftChildIndex;
+        }
+
+        if (rightChildIndex < elements.Count && comparer.Compare(elements[rightChildIndex], elements[smallest]) < 0)
+        {
+          smallest = rightChildIndex;
+        }
+
+        if (smallest != index)
+        {
+          Swap(index, smallest);
+          elementsDown(smallest);
+        }
+      }
+
+      private void Swap(int index1, int index2)
+      {
+        (elements[index1], elements[index2]) = (elements[index2], elements[index1]);
+        (chunkIndices[index1], chunkIndices[index2]) = (chunkIndices[index2], chunkIndices[index1]);
+        (elementIndices[index1], elementIndices[index2]) = (elementIndices[index2], elementIndices[index1]);
+      }
+
+    }
+
+    private T[] MergeSortedChunks(T[][] sortedChunks, int n, IComparer<T> comparer)
+    {
+      T[] result = new T[n];
+      var minElements = new MinElements<T, IComparer<T>>(comparer);
+
+      for (int i = 0; i < sortedChunks.Length; i++)
+      {
+        if (sortedChunks[i].Length > 0)
+        {
+          minElements.Add(sortedChunks[i][0], i, 0);
+        }
+      }
+
+      for (int i = 0; i < n; i++)
+      {
+        if (minElements.Count == 0)
+        {
+          break;
+        }
+
+        var (value, chunkIndex, elementIndex) = minElements.RemoveMin();
+        result[i] = value;
+
+        if (elementIndex + 1 < sortedChunks[chunkIndex].Length)
+        {
+          minElements.Add(sortedChunks[chunkIndex][elementIndex + 1], chunkIndex, elementIndex + 1);
+        }
+      }
+
+      return result;
+    }
+  }
+}
